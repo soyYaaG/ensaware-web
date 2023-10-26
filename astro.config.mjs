c8a753838d9fb77b8618f718f43da093e12c9985
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-10-25 22:59:31 -0500

    fix: add astro node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   image: {
-	service: passthroughImageService(),
+    service: passthroughImageService()
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });

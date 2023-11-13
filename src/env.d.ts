36a7881ab56705aa35c292951aeaa007b16b759a
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-11-13 16:17:52 -0500

    feat: add env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly PUBLIC_API_ENSAWARE: string;
+	readonly VERSION: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}

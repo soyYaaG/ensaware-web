7e2dbaaa550f3d5f1d6b5d017679e216afcca9e9
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-11-13 16:15:34 -0500

    feat: add config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,27 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  image: {
-    service: passthroughImageService()
-  },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+	site: "https://ensaware.yaag.pro",
+	integrations: [
+		tailwind(),
+		react(),
+		robotsTxt({
+			sitemap: true,
+		}),
+		sitemap(),
+	],
+	image: {
+		service: passthroughImageService(),
+	},
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
 });
